64895c701f5a10c07478d267578664bebb3cf3a1
stormy131 <artemkopyl1990@gmail.com>
2021-12-03 21:00:03 +0200

    auth module init

---
+++ web/src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService){}
+
+    @Get()
+    getLoginPage(){
+
+    }
+
+    @Post()
+    async postLogin(){
+
+    }
+
+    @Post()
+    async postReg(){
+        
+    }
+}
