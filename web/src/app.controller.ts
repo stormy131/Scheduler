1492f13a0fd9f41750f844a9bf7408af2574f816
stormy131 <artemkopyl1990@gmail.com>
2021-12-04 13:32:05 +0200

    refresh token

--- web/src/app.controller.ts
+++ web/src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  async refreshToken(@Req() req: Request): Promise<string>{
+    return await this.appService.refresh(req.headers.reftoken as string, req.headers.oldtoken as string);
   }
 }
--- web/src/app.service.ts
+++ web/src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { TokensRepo } from './database/repository/tokens.repository';
 
 @Injectable()
 export class AppService {
+  constructor(
+    private readonly tokensRepo: TokensRepo,
+    private readonly jwtService: JwtService
+  ){}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async refresh(refToken: string, oldToken: string): Promise<string>{
+    const res = await this.tokensRepo.findOne(refToken);
+    if(!res) return 'Forbidden resource';
+
+    const data = this.jwtService.decode(oldToken);
+    const newToken = this.jwtService.sign(data, {expiresIn: '1h'});
+    return newToken;
   }
 }
