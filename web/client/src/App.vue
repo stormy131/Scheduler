7a4dc486045694c2933437cbb0e0b7e6eaca2684
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-12-04 17:22:55 +0200

    Create client

---
+++ web/client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Main></Main>
+  </div>
+</template>
+
+<script>
+import Main from './components/Main.vue'
+
+export default {
+  name: 'Scheduler',
+  components: {
+    Main
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ web/client/src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Placeholder</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
