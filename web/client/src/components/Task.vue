7045aac7ecf01e013df85cf18a734156263565da
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-12-04 23:13:40 +0200

    заебало

--- web/client/src/components/Task.vue
+++ web/client/src/components/Task.vue
@@ -5,7 +5,7 @@
         <h4 class="main__task_pause" v-html="statuses[task.status]"></h4>
         <h4 class="task_title">{{ task.title }}}</h4>
       </div>
-      <button class="main__task_show-subtask">View Subtasks({{ task.subtasks.count }}}) <i class="fas fa-angle-down"></i></button>
+      <!--<button class="main__task_show-subtask">View Subtasks({{ task.subtasks.count }}}) <i class="fas fa-angle-down"></i></button>-->
     </div>
 <!--    <div class="main__task__subtasks" >
       <Subtask v-for="item of task.subtasks" :key="item.id" :subtask="item"></Subtask>
