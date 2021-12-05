01c63cd5c8f22cd71f69c53a7d7da413849365f6
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-12-05 03:34:30 +0200

    Компоненты ХУЙНИ

---
+++ web/client/src/components/ProjectItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="item">
+    <div class="name">{{ project.name }}</div>
+    <button class="activate"><div>Activate</div></button>
+    <div class="status">{{ project.status }}</div>
+    <div class="date">{{ project.deadline }}</div>
+    <div class="tasks">{{ project.tasks }}</div>
+    <button class="add">+ Add tasks</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectItem",
+  props: ['project'],
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ web/client/src/components/Projects.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="main">
+    <div class="title">Projects</div>
+    <ProjectItem v-for="project of projects" :key="project.id" project="project"></ProjectItem>
+    <div class="item new">
+      <div class="create"><span></span>Create Project</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProjectItem from './ProjectItem'
+import request from '../plugins/request'
+export default {
+  name: "Projects",
+  data() {
+    return {
+      projects: []
+    }
+  },
+  components: {
+    ProjectItem
+  },
+  mounted() {
+    request('/api/projects').then(data => {
+      this.projects = data;
+    })
+  }
+}
+</script>
+
+<style scoped>
+* {box-sizing: border-box;}
+
+.main {padding:28px;background:#E5E5E5;}
+.main .title {margin-bottom:18px;font-weight: 600;font-size: 25px;line-height: 30px;color: #000000;}
+
+.main .item {display:grid;grid-template-columns:repeat(6, minmax(0, 1fr));height:96px;margin-bottom:18px;padding:26px 42px;background: #FFFFFF;box-shadow: -2px -2px 50px -1px rgba(0, 0, 0, 0.03), 2px 2px 50px -1px rgba(0, 0, 0, 0.03);border-radius: 7px;}
+.main .item > div {display:flex;align-items: center;justify-content: center;font-family:Noto Sans,serif;font-weight: 600;font-size: 20px;line-height: 24px;color: #000000;}
+
+.main .item .name {justify-content: flex-start;}
+.main .item .activate div {width:fit-content;padding:10px 20px;background: #3C4DDB;border-radius: 7px;color: #FFFFFF;}
+.main .item .status {color:#5AC08F;}
+.main .item .date {color: rgba(0, 0, 0, 0.5);}
+.main .item .tasks {color:#3C4DDB;}
+.main .item .add  {color:#3C4DDB;}
+
+.main .item.active .activate div {display:none}
+
+.main .item.headings {height: 56px;padding:16px 42px;}
+.main .item.headings > div {color: rgba(0, 0, 0, 0.5);}
+
+.main .item.new {display:flex;justify-content:flex-start;padding:23px 42px;}
+.main .item.new .create {color:#3C4DDB;}
+.main .item.new .create span {position:relative;margin-right:48px;width:50px;height:50px;border-radius:50%;background:#3C4DDB}
+.main .item.new .create span:before {position:absolute;top:15px;left:25px;content:"";display:block;width:2px;height:20px;background:#fff}
+.main .item.new .create span:after {position:absolute;top:15px;left:25px;transform:rotate(90deg);content:"";display:block;width:2px;height:20px;background:#fff}
+</style>
