b6369322e2e05cda90004d10678921c862c7c4e2
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-12-05 03:24:28 +0200

    вот эта вот ХУЕТЕНЬ в гит не добавилась исправляю

---
+++ web/client/src/views/Main.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <Menu user="user"></Menu>
+    <Projects v-if="route === '/projects'"></Projects>
+    <Project v-else-if="route === '/projects'"> </Project>
+    <div>404 v-else</div>
+  </div>
+</template>
+
+<script>
+import Project from "../components/Project";
+import Menu from "../components/Menu";
+import Projects from "../components/Projects";
+import request from "../plugins/request.js"
+export default {
+  name: "Main",
+  components: {
+    Menu,
+    Project,
+    Projects,
+  },
+  data() {
+    return {
+      user: null
+    }
+  },
+  methods: {
+    setUser() {
+      request('/api/').then(data => {
+        this.user = data;
+      })
+    }
+  },
+  computed: {
+    route() {
+      return this.$route.path
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
