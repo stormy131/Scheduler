650f256ce39c1030881c536d5a7ff440cd6d9e25
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-12-31 21:46:51 +0200

    Mutations

--- web/client/src/store/store.js
+++ web/client/src/store/store.js
@@ -9,7 +9,23 @@ export default new Vuex.Store({
     token: localStorage.getItem('token') || '',
     user: {}
   },
-  mutations: {},
+  mutations: {
+    auth_request(state){
+      state.status = 'loading'
+    },
+    auth_success(state, token, user){
+      state.status = 'success'
+      state.token = token
+      state.user = user
+    },
+    auth_error(state){
+      state.status = 'error'
+    },
+    logout(state){
+      state.status = ''
+      state.token = ''
+    },
+  },
   actions: {
     login({ commit }, user) {
       return new Promise((resolve, reject) => {
