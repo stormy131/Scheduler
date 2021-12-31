ac8d27db007410e2990cfa4a1c42eebad4385d07
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-12-31 20:57:10 +0200

    Configure axios

--- web/client/src/main.js
+++ web/client/src/main.js
@@ -2,10 +2,17 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App.vue';
 import router from './router';
-import axios from 'axios'
+import store from './store';
+import Axios from 'axios';
 
 Vue.config.productionTip = false;
-window.axios = axios;
+
+Vue.prototype.$http = Axios;
+const token = localStorage.getItem('token');
+if (token) {
+    Vue.prototype.$http.defaults.headers.common['Authorization'] = token;
+}
+
 
 new Vue({
     render: h => h(App),
