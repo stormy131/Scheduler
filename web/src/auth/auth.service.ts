b700f010827662005b5bb64cd9327277cb84b7be
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-12-31 21:33:03 +0200

    Doing backender`s work

--- web/src/auth/auth.service.ts
+++ web/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 import { LoginInterface } from './interfaces/login.interface';
 import { RegistrationInterface } from './interfaces/register.interface';
 import { AccountsRepo } from '../database/repository/accounts.repository';
@@ -7,64 +7,77 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private readonly accountRepo: AccountsRepo,
-        private readonly tokenRepo: TokensRepo,
-        private readonly jwtService: JwtService
-    ){}
+  constructor(
+    private readonly accountRepo: AccountsRepo,
+    private readonly tokenRepo: TokensRepo,
+    private readonly jwtService: JwtService,
+  ) {}
 
-    async postLogin(loginData: LoginInterface): Promise<any>{
-        const res = await this.accountRepo.find(loginData);
+  async postLogin(loginData: LoginInterface): Promise<any> {
+    const res = await this.accountRepo.find(loginData);
 
-        if(!res) return 'Invalid user data';
+    if (!res) return 'Invalid user data';
 
-        const token = this.jwtService.sign({name: res.name, id: res.id}, {expiresIn: '1h'});
-        const refToken = this.jwtService.sign({username: res.name, id: res.id});
+    const token = this.jwtService.sign(
+      { name: res.name, id: res.id },
+      { expiresIn: '1h' },
+    );
+    const refToken = this.jwtService.sign({ username: res.name, id: res.id });
 
-        await this.tokenRepo.create(refToken);
+    const user = {
+      id: res.id,
+      email: res.email,
+      name: res.name,
+    };
 
-        return {
-            token,
-            refToken
-        };
+    await this.tokenRepo.create(refToken);
+
+    return {
+      token,
+      refToken,
+      user,
+    };
+  }
+
+  async postReg(newAccount: RegistrationInterface): Promise<string> {
+    const alreadyExists = await this.accountRepo.check(newAccount);
+
+    if (alreadyExists) {
+      return 'Provied email or username is already in use';
     }
 
-    async postReg(newAccount: RegistrationInterface): Promise<string>{
-        const alreadyExists = await this.accountRepo.check(newAccount);
+    await this.accountRepo.create(newAccount);
+    return 'created, now go to login';
+  }
 
-        if(alreadyExists){
-            return 'Provied email or username is already in use';
-        }
+  async refreshToken(oldToken: string, token: string) {
+    const res = await this.tokenRepo.findOne(token);
+    const data = this.jwtService.decode(oldToken);
 
-        await this.accountRepo.create(newAccount);
-        return 'created, now go to login';
-    }
+    if (res) return this.jwtService.sign(data, { expiresIn: '1h' });
+  }
 
-    async refreshToken(oldToken: string, token: string){
-        const res = await this.tokenRepo.findOne(token);
-        const data = this.jwtService.decode(oldToken);
+  async googleLogin(
+    request,
+  ): Promise<{ message: string; user: { email: string } }> {
+    const { user } = request;
 
-        if(res) return this.jwtService.sign(data, {expiresIn: '1h'});
-    }
+    if (!user)
+      return {
+        message: 'no google user',
+        user: {
+          email: '',
+        },
+      };
 
-    async googleLogin(request): Promise<{message: string, user: {email: string}}>{
-        const { user } = request;
+    user.name = user.username;
+    delete user.username;
+    user.password = '';
 
-        if(!user) return {
-            message: 'no google user',
-            user: {
-                email: ''
-            }
-        };
-
-        user.name = user.username;
-        delete user.username;
-        user.password = '';
-
-        await this.accountRepo.findOrCreate(user);
-        return {
-            message: 'User from google',
-            user
-        };
-    }
+    await this.accountRepo.findOrCreate(user);
+    return {
+      message: 'User from google',
+      user,
+    };
+  }
 }
