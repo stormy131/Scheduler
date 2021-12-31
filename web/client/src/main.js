a8f1d92dc532a61c3594df507288fc01ce3dac26
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-12-31 21:58:26 +0200

    add store to proto

--- web/client/src/main.js
+++ web/client/src/main.js
@@ -2,11 +2,12 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App.vue';
 import router from './router';
-//import store from './store';
+import store from './store';
 import Axios from 'axios';
 
 Vue.config.productionTip = false;
 
+Vue.prototype.$store = store;
 Vue.prototype.$http = Axios;
 const token = localStorage.getItem('token');
 if (token) {
