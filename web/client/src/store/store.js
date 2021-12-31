f162e244f18fa6409dca7740a73215f72cd11138
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-12-31 21:45:36 +0200

    Fix register

--- web/client/src/store/store.js
+++ web/client/src/store/store.js
@@ -37,18 +37,12 @@ export default new Vuex.Store({
     register({commit}, user){
       return new Promise((resolve, reject) => {
         commit('auth_request')
-        axios({url: 'http://localhost:3000/register', data: user, method: 'POST' })
+        axios({url: '/auth/reg', data: user, method: 'POST' })
           .then(resp => {
-            const token = resp.data.token
             const user = resp.data.user
-            localStorage.setItem('token', token)
-            axios.defaults.headers.common['Authorization'] = token
-            commit('auth_success', token, user)
-            resolve(resp)
+            resolve([resp, user]);
           })
           .catch(err => {
-            commit('auth_error', err)
-            localStorage.removeItem('token')
             reject(err)
           })
       })
