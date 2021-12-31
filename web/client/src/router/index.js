8608ac51967220b140d39d69a35a390b73e8c9e1
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-12-31 21:51:03 +0200

    Secured paths check

--- web/client/src/router/index.js
+++ web/client/src/router/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Main from "../views/Main";
-import SingleProject from "../views/SingleProject";
-import Login from "../views/Login";
-import Register from "../views/Register";
+import Main from '../views/Main';
+import SingleProject from '../views/SingleProject';
+import Login from '../views/Login';
+import Register from '../views/Register';
+import store from '../store';
 
 
 Vue.use(VueRouter);
@@ -40,5 +41,17 @@ const routes = [
 const router = new VueRouter({
     routes,
 });
+
+router.beforeEach((to, from, next) => {
+    if(to.matched.some(record => record.meta.requiresAuth)) {
+        if (store.getters.isLoggedIn) {
+            next()
+            return
+        }
+        next('/auth')
+    } else {
+        next()
+    }
+})
 
 export default router;
