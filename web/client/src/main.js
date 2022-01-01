86d507dd548b7b05ccce631d59135cd32a4c6510
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-01 02:22:30 +0200

    Token and user check

--- web/client/src/main.js
+++ web/client/src/main.js
@@ -10,8 +10,10 @@ Vue.config.productionTip = false;
 Vue.prototype.$store = store;
 Vue.prototype.$http = Axios;
 const token = localStorage.getItem('token');
-if (token) {
+const user = localStorage.getItem('user');
+if (token && user) {
   Vue.prototype.$http.defaults.headers.common.usertoken = token;
+  store.commit('auth_success', { token, user });
 }
 
 new Vue({
