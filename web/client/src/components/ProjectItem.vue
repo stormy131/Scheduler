2586e7df5844263d62bf492e5676fcb086ff64f2
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-02 03:55:38 +0200

    Project item fixes

--- web/client/src/components/ProjectItem.vue
+++ web/client/src/components/ProjectItem.vue
@@ -2,12 +2,12 @@
   <div class="item">
     <div class="name">{{ project.name }}</div>
     <button class="activate">
-      <span v-show="!(project.status === 'activated')">Activate</span>
+      <span v-show="!(project.status === true)">Activate</span>
     </button>
-    <div class="status">{{ project.status }}</div>
-    <div class="date">{{ project.deadline }}</div>
+    <div class="status">{{ project.active }}</div>
+    <div class="date">{{ project.createdAt }}</div>
     <div class="tasks">{{ project.tasks }}</div>
-    <button class="add">+ Add tasks</button>
+    <button class="add" @click="log">+ Add tasks</button>
   </div>
 </template>
 
@@ -15,9 +15,18 @@
 export default {
   name: "ProjectItem",
   props: ['project'],
+  mounted() {
+  },
+  methods: {
+    log() {
+      console.log(this.project);
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  .item {
+    align-items: center;
+  }
 </style>
