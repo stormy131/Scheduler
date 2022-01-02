38a15e12053f2d7affd7899f19ab525a033641f6
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-02 18:23:20 +0200

    Promise => async login

--- web/client/src/store/index.js
+++ web/client/src/store/index.js
@@ -1,83 +1,88 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import axios from 'axios';
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
-    status: '',
-    token: localStorage.getItem('token') || '',
+    status: "",
+    token: localStorage.getItem("token") || "",
     user: {}
   },
   mutations: {
     auth_request(state) {
-      state.status = 'loading';
+      state.status = "loading";
     },
     auth_success(state, payload) {
-      state.status = 'success';
+      state.status = "success";
       state.token = payload.token;
       state.user = payload.user;
     },
     auth_error(state) {
-      state.status = 'error';
+      state.status = "error";
     },
     logout(state) {
-      state.status = '';
-      state.token = '';
+      state.status = "";
+      state.token = "";
       state.user = {};
     }
   },
   actions: {
-    login({ commit }, user) {
-      return new Promise((resolve, reject) => {
-        commit('auth_request');
-        axios({
-          url: '/auth',
+    async login({ commit }, user) {
+      commit("auth_request");
+      try {
+        const r = await axios({
+          url: "/auth",
           data: user,
-          method: 'POST'
-        })
-          .then(r => {
-            const token = r.data.token;
-            const user = r.data.user;
-            localStorage.setItem('token', token);
-            localStorage.setItem('user', user);
-            axios.defaults.headers.common.usertoken = token;
-            commit('auth_success', { token, user });
-            resolve(r);
-          })
-          .catch(err => {
-            commit('auth_error');
-            localStorage.removeItem('token');
-            localStorage.removeItem('user');
-            reject(err);
-          });
-      });
+          method: "POST"
+        });
+        const token = r.data.token;
+        const user = r.data.user;
+        localStorage.setItem("token", token);
+        localStorage.setItem("user", user);
+        axios.defaults.headers.common.usertoken = token;
+        commit("auth_success", { token, user });
+        return r;
+      } catch (err) {
+        commit("auth_error");
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        throw err;
+      }
     },
-    register({ commit }, user) {
-      return new Promise((resolve, reject) => {
-        commit('auth_request');
-        axios({ url: '/auth/reg', data: user, method: 'POST' })
-          .then(resp => {
-            resolve(resp);
-          })
-          .catch(err => {
-            reject(err);
-          });
-      });
+      register({ commit }, user)
+      {
+        return new Promise((resolve, reject) => {
+          commit("auth_request");
+          axios({ url: "/auth/reg", data: user, method: "POST" })
+            .then(resp => {
+              resolve(resp);
+            })
+            .catch(err => {
+              reject(err);
+            });
+        });
+      }
+    ,
+      logout({ commit });
+      {
+        return new Promise((resolve) => {
+          commit("logout");
+          localStorage.removeItem("token");
+          localStorage.removeItem("user");
+          delete axios.defaults.headers.usertoken;
+          resolve();
+        });
+      }
     },
-    logout({ commit }) {
-      return new Promise((resolve) => {
-        commit('logout');
-        localStorage.removeItem('token');
-        localStorage.removeItem('user');
-        delete axios.defaults.headers.usertoken;
-        resolve();
-      });
-    }
-  },
-  getters: {
-    isLoggedIn: state => !!state.token,
-    authStatus: state => state.status,
-    user: state => state.user
-  }
-});
+    getters: {
+      isLoggedIn: state => !!state.token,
+      authStatus;
+:
+state => state.status,
+  user;
+:
+state => state.user;
+}
+})
+;
