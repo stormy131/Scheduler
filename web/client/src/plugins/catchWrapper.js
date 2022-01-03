dab00cf1a681387412fba644769fe208b0db8b5c
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-03 12:45:34 +0200

    add args

--- web/client/src/plugins/catchWrapper.js
+++ web/client/src/plugins/catchWrapper.js
@@ -1,5 +1,5 @@
-const catchWrapper = (func, handler, args) => {
-  return func(args).catch((error) => handler(error));
+const catchWrapper = (func, handler, ...args) => {
+  return func(...args).catch((error) => handler(error));
 };
 
 export default catchWrapper;
