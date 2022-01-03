926bb8eeb2acf1b4fe6723a35b0437ea82891f84
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-03 10:56:42 +0200

    Get project on mount

--- web/client/src/views/SingleProject.vue
+++ web/client/src/views/SingleProject.vue
@@ -18,8 +18,15 @@ export default {
   data() {
     return {
       user: this.$store.getters.user,
+      project: null,
     };
-  }
+  },
+  mounted() {
+    this.$http
+      .get(`/projects/${this.$route.params.id}`)
+      .then((resp) => (this.project = resp.data))
+      .catch((err) => console.log(err));
+  },
 };
 </script>
 
