9ac7b341a29763c7b63ab3f30d16e8a3b95d9f3e
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-03 11:29:36 +0200

    Fix mounted

--- web/client/src/components/Project.vue
+++ web/client/src/components/Project.vue
@@ -1,46 +1,46 @@
 <template>
-  <main class="main">
-    <header class="main__header">
-      <div class="header__left">
-        <h3 class="main__heading">{{ project.name }}</h3>
-        <button class="main__edit" @click="isEdit = !isEdit">Edit</button>
-        <router-link class="main__projects" to="{path: '/projects'}}">
-          <i class="fas fa-chevron-left"></i>
+  <main class='main'>
+    <header class='main__header'>
+      <div class='header__left'>
+        <h3 class='main__heading'>{{ project.name }}</h3>
+        <button class='main__edit' @click='isEdit = !isEdit'>Edit</button>
+        <router-link class='main__projects' to="{path: '/projects'}}">
+          <i class='fas fa-chevron-left'></i>
           All projects
         </router-link>
       </div>
-      <div class="header__right">
-        <form action="#">
+      <div class='header__right'>
+        <form action='#'>
           <label>
-            <input class="main__search" placeholder="Search..." type="text" />
+            <input class='main__search' placeholder='Search...' type='text' />
           </label>
-          <button class="main__submit-search" type="submit"><i class="fas fa-search"></i></button>
+          <button class='main__submit-search' type='submit'><i class='fas fa-search'></i></button>
         </form>
       </div>
     </header>
-    <div class="main__sort-tasks">
-      <div class="main__sort-tasks__pages">
-        <button class="main__sort-tasks-page">Tasks</button>
+    <div class='main__sort-tasks'>
+      <div class='main__sort-tasks__pages'>
+        <button class='main__sort-tasks-page'>Tasks</button>
       </div>
-      <div class="main__sort-tasks__buttons">
-        <button class="main__sort-tasks__button" @click="isCreate = !isCreate">+ Create Task</button>
+      <div class='main__sort-tasks__buttons'>
+        <button class='main__sort-tasks__button' @click='isCreate = !isCreate'>+ Create Task</button>
       </div>
     </div>
-    <div class="main__tasks">
-      <p class="main__par main__tasks_count">
-        <span class="active">{{ projectCount }}</span> items in result
+    <div class='main__tasks'>
+      <p class='main__par main__tasks_count'>
+        <span class='active'>{{ projectCount }}</span> items in result
       </p>
-      <div class="main__tasks-body">
-        <Task v-for="task of tasks" :key="task.id" :task="task"></Task>
-        <div class="create__task">
-          <button class="create__task_button">
-            <i class="fas fa-plus"></i>
+      <div class='main__tasks-body'>
+        <Task v-for='task of tasks' :key='task.id' :task='task'></Task>
+        <div class='create__task'>
+          <button class='create__task_button'>
+            <i class='fas fa-plus'></i>
           </button>
-          <p class="create__task__text" @click="isCreate = !isCreate">Create task</p>
+          <p class='create__task__text' @click='isCreate = !isCreate'>Create task</p>
         </div>
       </div>
     </div>
-    <NewTask v-show="isEdit" @created="newTask($event)"></NewTask>
+    <NewTask v-show='isEdit' @created='newTask($event)'></NewTask>
   </main>
 </template>
 
@@ -50,19 +50,19 @@ import NewTask from './NewTask';
 
 export default {
   props: {
-    project: Object,
+    project: Object
   },
   name: 'Project',
   data() {
     return {
       tasks: [],
       isEdit: false,
-      isCreate: false,
+      isCreate: false
     };
   },
   components: {
     Task,
-    NewTask,
+    NewTask
   },
   methods: {
     editHandler() {
@@ -86,16 +86,12 @@ export default {
   computed: {
     projectCount() {
       return this.tasks.length;
-    },
+    }
   },
   mounted() {
-    this.$http.get(`/project/${this.project.id}`)
-      .then((resp) => {
-        console.log(resp);
-        this.tasks = [...resp.data];
-      })
-      .catch((err) => console.log(err));
-  },
+    this.tasks = [this.project.tasks];
+    console.log(this.project, this.tasks);
+  }
 };
 </script>
 
