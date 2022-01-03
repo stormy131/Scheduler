b4fac9b092ed89b95e670f4ce374193f7dc5b8fe
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-03 10:15:57 +0200

    Delegate delete and create methods to Project

--- web/client/src/components/ProjectItem.vue
+++ web/client/src/components/ProjectItem.vue
@@ -7,7 +7,8 @@
     <div class="status" :class="[project.active ? 'true' : 'false']">{{ project.active }}</div>
     <div class="date">{{ project.createdAt }}</div>
     <div class="tasks">{{ project.tasks }}</div>
-    <button class="add" @click="click">+ Add tasks</button>
+    <button class="add" @click="addTask">+ Add tasks</button>
+    <button class='close-button' @click="deleteItem">&times;</button>
   </div>
 </template>
 
@@ -18,16 +19,15 @@ export default {
     project: Object,
   },
   methods: {
-    click() {
+    addTask() {
       this.$emit('addTask', this.project.id);
     },
     updateStatus() {
-      this.$http.patch(`/projects/${this.project.id}`)
-        .then(() => {
-          this.project.active = !this.project.active;
-          this.$emit('changeStatus', this.project.id);
-        }).catch(err => console.log(err));
+      this.$emit('changeStatus', this.project.id);
     },
+    deleteItem() {
+      this.$emit('projectDeleted', this.project.id);
+    }
   },
 };
 </script>
